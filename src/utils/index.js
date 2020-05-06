6e22ae4ad9bf31dcf4d6d1f3991684a85be4af8c
solophone <solophone@aliyun.com>
2020-05-06 23:55:01 +0800

    添加util工具库

---
+++ src/utils/index.js
@@ -0,0 +1,16 @@
+// 有一个组件，向上找到最近的一个指定组件
+function findComponentUpward(context, componentName) {
+  let parent = context.$parent;
+  let name = parent.$options.name;
+  while (parent && (!name || name !== componentName)) {
+    parent = parent.$parent;
+    if (parent) {
+      name = parent.$options.name;
+    }
+  }
+  return parent;
+}
+
+export default {
+  findComponentUpward,
+};
